2aee44792d839744374b944c330ff160c1fe26bf
Mauricekinyua <maurice.k.muchiri@gmail.com>
2025-06-23 18:41:05 +0300

    T

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import gsap from 'gsap'
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+    </main>
+  )
+}
+
+export default App
